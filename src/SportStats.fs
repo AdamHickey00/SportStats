894416bb837e22e62187b0e45d41a21b10e9babf
AdamHickey00 <adam.hickey00@gmail.com>
2015-12-30 23:39:09 -0500

    Add web part

--- src/SportStats.fs
+++ src/SportStats.fs
@@ -7,12 +7,23 @@ open Suave.Http.Successful
 open Suave.Types
 open Suave.Web
 
-//let routes =
-//  choose
-//    [ GET >>=
-//      choose [ pathScan "/Golf/HoleInOnes/%s" >>= ]
+let getName (ctx : HttpContext) =
+  let r = ctx.request
+  (r.queryParam "firstName"), (r.queryParam "lastName")
+
+let getHoleInOnes (ctx : HttpContext) =
+  let firstNameChoice = fst (getName ctx)
+  let lastNameChoice = snd (getName ctx)
+
+  (OK "Hello 3") ctx
+
+let routes =
+  choose
+    [ GET >>=
+      choose [ path "/Golf/HoleInOnes" >>= getHoleInOnes ]]
 
 [<EntryPoint>]
 let main argv =
-    startWebServer defaultConfig (OK "Hello World22!")
+    //startWebServer defaultConfig (OK "Hello World22!")
+    startWebServer defaultConfig routes
     0
