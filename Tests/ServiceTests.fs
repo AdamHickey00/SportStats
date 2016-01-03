2618ef643ec2c724ba01b0d235b77ea4f587f91e
AdamHickey00 <adam.hickey00@gmail.com>
2016-01-03 14:02:49 -0500

    Update spacing

--- Tests/ServiceTests.fs
+++ Tests/ServiceTests.fs
@@ -11,31 +11,31 @@ let getResponse content =
 
 [<Fact>]
 let ``Golf hole in ones Tiger Woods``() =
-    let expectedResponse = "{\"FirstName\":\"Tiger\",\"LastName\":\"Woods\",\"Accomplishment\":{\"Case\":\"HoleInOnes\",\"Fields\":[7]}}"
+  let expectedResponse = "{\"FirstName\":\"Tiger\",\"LastName\":\"Woods\",\"Accomplishment\":{\"Case\":\"HoleInOnes\",\"Fields\":[7]}}"
 
-    let webPart = SportStats.routes
-    let query = "firastName=Tiger&lastName=Woods"
-    let host = "http://localhost/Golf/HoleInOnes?" + query
-    let request =
-      { HttpRequest.empty with
-          ``method`` = HttpMethod.GET
-          url = new System.Uri(host); rawQuery = query }
+  let webPart = SportStats.routes
+  let query = "firastName=Tiger&lastName=Woods"
+  let host = "http://localhost/Golf/HoleInOnes?" + query
+  let request =
+    { HttpRequest.empty with
+        ``method`` = HttpMethod.GET
+        url = new System.Uri(host); rawQuery = query }
 
-    let context = { HttpContext.empty with request = request }
-    let result = webPart context |> Async.RunSynchronously
+  let context = { HttpContext.empty with request = request }
+  let result = webPart context |> Async.RunSynchronously
 
-    match result with
-    | None -> failwith "Route not found"
-    | Some ctx ->
+  match result with
+  | None -> failwith "Route not found"
+  | Some ctx ->
 
-        // response good
-        ctx.response.status |> should equal HttpCode.HTTP_200
+      // response good
+      ctx.response.status |> should equal HttpCode.HTTP_200
 
-        // contains json header
-        ctx.response.headers
-        |> List.exists(fun (k,v) -> k = "Content-Type" && v = "application/json")
-        |> should equal true
+      // contains json header
+      ctx.response.headers
+      |> List.exists(fun (k,v) -> k = "Content-Type" && v = "application/json")
+      |> should equal true
 
-        // check resonse
-        getResponse ctx.response.content
-        |> should equal expectedResponse
+      // check resonse
+      getResponse ctx.response.content
+      |> should equal expectedResponse
