a4487645a55d9b7c15af7f3c851d6a7e5d4c9a34
Adam Hickey <adam.hickey00@gmail.com>
2016-01-06 11:53:01 -0500

    Return 0 if athlete not found

--- src/Database.fs
+++ src/Database.fs
@@ -13,17 +13,23 @@ let getLowestTournament firstName lastName : Athlete =
 
   let url = sprintf "http://www.golfstats.com/search/?stat=6&player=%s+%s&submit=go" firstName lastName
   let stats = LowestTournamentPage.Load(url)
+  let tables = stats.Html.Descendants ["table"]
 
-  let lowestScore =
-    stats.Html.Descendants ["table"]
-    |> Seq.head
-    |> (fun x -> (x.Descendants ["tr"]).ElementAt(rowPosition))
-    |> (fun x -> (x.Descendants ["td"]).ElementAt(columnPosition))
-    |> (fun x -> x.InnerText())
+  match Seq.length tables with
+  | 0 -> { FirstName = firstName
+           LastName = lastName
+           Stat = LowestTournament (int 0) }
 
-  { FirstName = firstName
-    LastName = lastName
-    Stat = LowestTournament (int lowestScore) }
+  | _ -> let lowestScore = 
+           tables
+           |> Seq.head
+           |> (fun x -> (x.Descendants ["tr"]).ElementAt(rowPosition))
+           |> (fun x -> (x.Descendants ["td"]).ElementAt(columnPosition))
+           |> (fun x -> x.InnerText())
+
+         { FirstName = firstName
+           LastName = lastName
+           Stat = LowestTournament (int lowestScore) }
 
 let DB =
   { new IDB with
