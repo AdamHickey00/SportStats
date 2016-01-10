caebaf0087c2ecdc218951230edbae96b8f7d656
Adam Hickey <adam.hickey00@gmail.com>
2016-01-10 16:55:39 -0500

    Refactor database function

--- src/Database.fs
+++ src/Database.fs
@@ -6,8 +6,47 @@ open Types
 
 type LowestTournamentPage = HtmlProvider<"http://www.golfstats.com/search/?stat=6&player=Tiger+Woods&submit=go">
 
-let getLowestTournament firstName lastName : Athlete =
+let lowestRoundMap (columnPosition:int) (row:HtmlNode) : int =
+  let columnValue = (row.Descendants ["td"]).ElementAt(columnPosition).InnerText()
 
+  // value like "66-61-68-70=265"
+  (columnValue.Split [|'='|]).[0].Split [|'-'|]
+  |> Array.map (fun x -> int x)
+  |> Array.min
+
+//let lowestTournamentMap (columnPosition:int) (row:HtmlNode) : int =
+//  let columnValue = (row.Descendants ["td"]).ElementAt(columnPosition).InnerText()
+
+  // value like "66-61-68-70=265"
+//  (columnValue.Split [|'='|]).[0].Split [|'-'|]
+//  |> Array.map (fun x -> int x)
+//  |> Array.min
+
+let getGolfStat firstName lastName columnPosition mapFunc filterFunc : Athlete =
+  let url = sprintf "http://www.golfstats.com/search/?stat=6&player=%s+%s&submit=go" firstName lastName
+  let stats = LowestTournamentPage.Load(url)
+  let tables = stats.Html.Descendants ["table"]
+
+  match Seq.length tables with
+  | 0 -> { FirstName = firstName
+           LastName = lastName
+           Stat = LowestTournament (int 0) }
+
+  | _ -> let lowestRound =
+           tables
+           |> Seq.head
+           |> (fun x -> x.Descendants ["tbody"])
+           |> Seq.head
+           |> (fun x -> (x.Descendants ["tr"]))
+           |> Seq.map (mapFunc columnPosition)
+           |> Seq.filter filterFunc
+           |> Seq.min
+
+         { FirstName = firstName
+           LastName = lastName
+           Stat = LowestRound lowestRound }
+
+let getLowestTournament firstName lastName : Athlete =
   let url = sprintf "http://www.golfstats.com/search/?stat=6&player=%s+%s&submit=go" firstName lastName
   let stats = LowestTournamentPage.Load(url)
   let tables = stats.Html.Descendants ["table"]
@@ -30,37 +69,11 @@ let getLowestTournament firstName lastName : Athlete =
            LastName = lastName
            Stat = LowestTournament (int lowestScore) }
 
+let getLowestTournament2 firstName lastName : Athlete =
+  getGolfStat firstName lastName 3 lowestRoundMap (fun (x:int) -> x > 50)
+
 let getLowestRound firstName lastName : Athlete =
-  let url = sprintf "http://www.golfstats.com/search/?stat=6&player=%s+%s&submit=go" firstName lastName
-  let stats = LowestTournamentPage.Load(url)
-  let tables = stats.Html.Descendants ["table"]
-  let columnPosition = 2
-
-  match Seq.length tables with
-  | 0 -> { FirstName = firstName
-           LastName = lastName
-           Stat = LowestTournament (int 0) }
-
-  | _ -> let lowestRound =
-           tables
-           |> Seq.head
-           |> (fun x -> x.Descendants ["tbody"])
-           |> Seq.head
-           |> (fun x -> (x.Descendants ["tr"]))
-           |> Seq.map (fun x ->
-                        let columnValue = (x.Descendants ["td"]).ElementAt(columnPosition).InnerText()
-
-                        // value like "66-61-68-70=265"
-                        (columnValue.Split [|'='|]).[0].Split [|'-'|]
-                        |> Array.map (fun x -> int x)
-                        |> Array.min
-                      )
-           |> Seq.filter (fun x -> x > 50) // filter out bad data
-           |> Seq.min
-
-         { FirstName = firstName
-           LastName = lastName
-           Stat = LowestRound lowestRound }
+  getGolfStat firstName lastName 2 lowestRoundMap (fun (x:int) -> x > 50)
 
 let DB =
   { new IDB with
