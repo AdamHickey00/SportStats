09a0ce4b2da49a909518760353dc1715c978e1cc
Adam Hickey <adam.hickey00@gmail.com>
2016-01-16 10:04:09 -0500

    Update client

--- client/SportStats.js
+++ client/SportStats.js
@@ -1,15 +1,15 @@
-function getStat() {
+function getStat(endpoint) {
   var request = new XMLHttpRequest();
   var firstName = document.getElementById('first').value;
   var lastName = document.getElementById('last').value;;
 
-  var url = "http://127.0.0.1:8083/Golf/TotalEarnings?firstName=" + firstName + "&lastName=" + lastName;
+  var url = "http://127.0.0.1:8083/Golf/" + endpoint + "?firstName=" + firstName + "&lastName=" + lastName;
 
   request.onreadystatechange = function() {
     if (request.readyState == 4 && request.status == 200) {
       var response = JSON.parse(request.responseText);
       var statValue = response.Stat.Fields[0];
-      document.getElementById("stat").innerHTML = statValue;
+      document.getElementById(endpoint).innerHTML = statValue;
     }
   };
 
