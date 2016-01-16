4d1b2048e093b9047313dfcba7a48e645f7e8b70
Adam Hickey <adam.hickey00@gmail.com>
2016-01-16 09:56:31 -0500

    Wire up first and last names

--- client/SportStats.js
+++ client/SportStats.js
@@ -1,6 +1,9 @@
 function getStat() {
   var request = new XMLHttpRequest();
-  var url = "http://127.0.0.1:8083/Golf/TotalEarnings?firstName=Kevin&lastName=Kisner";
+  var firstName = document.getElementById('first').value;
+  var lastName = document.getElementById('last').value;;
+
+  var url = "http://127.0.0.1:8083/Golf/TotalEarnings?firstName=" + firstName + "&lastName=" + lastName;
 
   request.onreadystatechange = function() {
     if (request.readyState == 4 && request.status == 200) {
