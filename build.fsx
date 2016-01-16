3e59f9a58c7721c5685ef2b1c28790997604ce27
Adam Hickey <adam.hickey00@gmail.com>
2016-01-16 09:44:00 -0500

    Update build script

--- build.fsx
+++ build.fsx
@@ -11,7 +11,7 @@ let appReferences = !! "*.sln"
 // Targets
 let clean _ = CleanDirs [buildDir]
 
-let instal  l () =
+let install () =
     let execParams : Fake.ProcessHelper.ExecParams = {
         Program = "./.paket/paket.exe"
         CommandLine = "install"
